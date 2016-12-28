a603a9c33f322dacac3d3f6bb29404c61e6afa7b
CroniX61 <animysteria@gmail.com>
2016-12-28 22:16:12 +0600

    Add files via upload

---
+++ menuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class menuController : MonoBehaviour {
+
+    public Button Play;
+    public Button Quit;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+    public void Play_Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit_Game()
+    {
+        Application.Quit();
+    }
+}
